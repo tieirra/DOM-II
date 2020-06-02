710ea7bf4e2bffc8687e9e5e01b04664088165ed
user <user@users-MacBook-Air.local>
2020-06-02 15:46:22 -0500

    adding code for mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let h2 = document.getElementsByName(h2);
+
+h2.addEventListener("mouseover",changeColorOnMouseOver);
+
+function changeColorOnMouseOver(){
+   this.style.background = 'hot pink';
+}
